1ee6523e426ea0122667d10869a698347cdcb1c3
hkarthikraj01 <53332719+hkarthikraj01@users.noreply.github.com>
2020-12-19 20:29:46 +0530

    Update app.js

--- admin/app.js
+++ admin/app.js
@@ -4,20 +4,27 @@ const cafeList = document.querySelector('#cafe-list');
 function renderCafe(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
-    let city = document.createElement('span');
+    let email = document.createElement('span');
+    let phone = document.createElement('span');
+    let date = document.createElement('span');
 
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
-    city.textContent = doc.data().city;
+    email.textContent = doc.data().email;
+     phone.textContent = doc.data().phone;
+     date.textContent = doc.data().date;
+    
 
     li.appendChild(name);
-    li.appendChild(city);
+    li.appendChild(email);
+     li.appendChild(phone);
+    li.appendChild(date);
 
     cafeList.appendChild(li);
 }
 
 // getting data
-db.collection('cafes').get().then(snapshot => {
+db.collection('users').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
         renderCafe(doc);
     });

38c02eb258c9d979f94f356c18d128f8b85e116a
hkarthikraj01 <53332719+hkarthikraj01@users.noreply.github.com>
2020-12-19 20:04:51 +0530

    Update form.js

--- form.js
+++ form.js
@@ -19,8 +19,8 @@ window.onload=function () {
   render();
 };
 function render() {
-    window.recaptchaVerifier=new firebase.auth.RecaptchaVerifier('recaptcha-container');
-    recaptchaVerifier.render();
+    //window.recaptchaVerifier=new firebase.auth.RecaptchaVerifier('recaptcha-container');
+    //recaptchaVerifier.render();
 }
 function phoneAuth() {
     //get the number
